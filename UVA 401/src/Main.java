dc2ab2b85416738349ecdd7bf4f4dfc6dc44a4fe
Shawon Ashraf <shawon13@live.com>
2017-01-11 01:45:15 +0600

    Done 401

--- UVA 401/src/Main.java
+++ UVA 401/src/Main.java
@@ -10,13 +10,23 @@ public class Main {
 
         Map<Character, Character> map = new HashMap<>();
         map.put('A', 'A');
+        map.put('B', 'A');
+        map.put('C', 'A');
+        map.put('D', 'A');
         map.put('E', '3');
+        map.put('F', '3');
+        map.put('G', '3');
         map.put('H', 'H');
         map.put('I', 'I');
         map.put('J', 'L');
+        map.put('K', 'L');
         map.put('L', 'J');
         map.put('M', 'M');
+        map.put('N', 'M');
         map.put('O', 'O');
+        map.put('P', 'O');
+        map.put('Q', 'O');
+        map.put('R', 'O');
         map.put('S', '2');
         map.put('T', 'T');
         map.put('U', 'U');
@@ -28,8 +38,12 @@ public class Main {
         map.put('1', '1');
         map.put('2', 'S');
         map.put('3', 'E');
+        map.put('4', 'E');
         map.put('5', 'Z');
+        map.put('6', 'Z');
+        map.put('7', 'Z');
         map.put('8', '8');
+        map.put('9', '8');
 
 
 
@@ -41,8 +55,13 @@ public class Main {
             isPal = isPalindrome(s);
             isM = isMirror(s, map);
 
-            System.out.println("isPal = " + isPal + "   isM = " + isM);
+            if(!isPal && !isM) System.out.println(s + " -- is not a palindrome.\n");
+            else if(isPal && !isM) System.out.println(s + " -- is a regular palindrome.\n");
+            else if(!isPal && isM) System.out.println(s + " -- is a mirrored string.\n");
+            else System.out.println(s + " -- is a mirrored palindrome.\n");
         }
+
+        in.close();
     }
 
 
@@ -63,11 +82,24 @@ public class Main {
 
         int l = s.length();
         char[] cr = new char[l];
+        String r = new StringBuilder(s).reverse().toString();
 
         for(int i = 0; i < l; i++) {
+            char c = s.charAt(i);
+            if(c == '0') continue;
 
+            char rev = map.get(c);
+
+            cr[i] = rev;
         }
 
-        return false;
+        String m = new String(cr);
+
+        if(r.equals(m)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
